f710fa7e9160f36b2f955973f39ef3260df5bb2f
Craig Dempsey <craigdempsey@gmail.com>
2017-04-28 15:14:22 -0400

    form working except remove button

--- planda2/app/controllers/comments_controller.rb
+++ planda2/app/controllers/comments_controller.rb
@@ -1,11 +1,19 @@
 class CommentsController < ApplicationController
+
   before_action :authenticate_user!
 
   def create
+    Comment.create(
+      content: params[:content],
+      task_id: params[:task_id])
+    redirect_to (:back)
   end
 
   def destroy
     Comment.destroy(params[:id])
     redirect_to "/"
   end
+
 end
+
+
--- planda2/db/seeds.rb
+++ planda2/db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Task.destroy_all
+Comment.destroy_all
+
+
+Task.create(name:"Gym",frequency:"Daily",completeness_level:0,user_id:1)
+Task.create(name:"Clean Bathroom",frequency:"Weekly",completeness_level:0,user_id:1)
+Task.create(name:"Tanning",frequency:"Daily",completeness_level:0,user_id:1)
+Task.create(name:"Laundry",frequency:"Weekly",completeness_level:0,user_id:1)
+Task.create(name:"Homework",frequency:"Daily",completeness_level:0,user_id:1)
+Task.create(name:"Walk the Dog",frequency:"Daily",completeness_level:0,user_id:1)
+Comment.create(content:"OVERDONE",task_id:3)
