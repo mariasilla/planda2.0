3d8e964d2227085f69aec5d2bab51817a8a7647c
Craig Dempsey <craigdempsey@gmail.com>
2017-04-29 14:30:33 -0400

    new team form adds team to db yay!!

--- planda2/app/controllers/teams_controller.rb
+++ planda2/app/controllers/teams_controller.rb
@@ -1,23 +1,37 @@
 class TeamsController < ApplicationController
-  before_action :authenticate_user!
+ before_action :authenticate_user!
+
   def index
   end
 
   def show
   end
 
-  def new
-  end
-
-  def create
-  end
-
   def edit
+    @team = Team.find(params[:id])
   end
 
   def update
+    @team=Team.find(params[:id])
+    @team.update(name: params[:name], frequency: params[:frequency], completeness_level: params[:completeness_level])
+       redirect_to "/team"
+  end
+
+  def new
+    @team = Team.new
+  end
+
+  def create
+    @user = current_user[:id]
+    @team = Team.create(name: params[:team_name])
+    Grouping.create(user_id: current_user[:id],
+      team_id: @team[:id])
+    redirect_to "/teams"
   end
 
   def destroy
+    Team.destroy(params[:id])
+    redirect_to "/"
   end
 end
+
