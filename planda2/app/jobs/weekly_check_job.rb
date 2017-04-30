2ccad51fa81805bf34a0f0edd5f29916bfbfe267
Scott Kaplan <scottkaplan.sk@gmail.com>
2017-04-30 10:38:18 -0400

    added edit and delete functionallity for teams

---
+++ planda2/app/jobs/weekly_check_job.rb
@@ -0,0 +1,7 @@
+class WeeklyCheckJob < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+    # Do something later
+  end
+end
---
+++ planda2/test/jobs/daily_check_job_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class DailyCheckJobTest < ActiveJob::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
