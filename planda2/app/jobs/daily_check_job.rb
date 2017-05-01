2ade45b8d0e585d1d115d7d27b71bdc7a62ff9e7
Scott Kaplan <scottkaplan.sk@gmail.com>
2017-05-01 09:39:42 -0400

    styling with materialize

--- planda2/app/jobs/daily_check_job.rb
+++ planda2/app/jobs/daily_check_job.rb
@@ -2,6 +2,12 @@ class DailyCheckJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    # Do something later
+    if task.frequency == 'daily'
+      if task.completeness == 'done'
+        task.completed++
+      end
+      task.completeness=='not started'
+      task.cycles++
+    end
   end
 end
