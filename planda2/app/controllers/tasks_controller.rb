634efa44113e46bce49b15ce3f6c6ee87c438fb0
erin fox <erinkayfox@gmail.com>
2017-05-02 16:54:22 -0400

    fixed

--- planda2/app/controllers/tasks_controller.rb
+++ planda2/app/controllers/tasks_controller.rb
@@ -22,6 +22,9 @@ end
     @comments=Comment.where(task_id: params[:id])
     @team_id=@task.team_id
     @team=Team.find(@team_id)
+    @user_id=@task.user_id
+    @user=User.find(@user_id)
+
     @playlistArray = ["https://open.spotify.com/embed?uri=spotify:user:spotify:playlist:37i9dQZF1DWUNIrSzKgQbP","https://open.spotify.com/embed?uri=spotify:user:spotify:playlist:37i9dQZF1DWU0ScTcjJBdj","https://open.spotify.com/embed?uri=spotify:user:spotify:playlist:37i9dQZF1DX7KNKjOK0o75","https://open.spotify.com/embed?uri=spotify:user:spotify:playlist:37i9dQZF1DX6ziVCJnEm59","https://open.spotify.com/embed?uri=spotify:user:spotify:playlist:37i9dQZF1DWSqmBTGDYngZ","https://open.spotify.com/embed?uri=spotify:user:spotify:playlist:37i9dQZF1DX9XIFQuFvzM4","https://open.spotify.com/embed?uri=spotify:user:spotify:playlist:37i9dQZF1DX6z20IXmBjWI","https://open.spotify.com/embed?uri=spotify:user:spotify:playlist:37i9dQZF1DX82pCGH5USnM","https://open.spotify.com/embed?uri=spotify:user:sonymusic:playlist:6bfxfMIcYKN4ce6XQOxoqY","https://open.spotify.com/embed?uri=spotify:user:spotify:playlist:37i9dQZF1DWVu0D7Y8cYcs"]
     @randomPlaylist = @playlistArray.sample
 
