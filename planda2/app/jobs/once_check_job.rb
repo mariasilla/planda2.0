2d72288e39bbcdc95611c35a69ae40759252becc
erin fox <erinkayfox@gmail.com>
2017-05-03 10:39:13 -0400

    fixes

--- planda2/app/jobs/once_check_job.rb
+++ planda2/app/jobs/once_check_job.rb
@@ -1,10 +1,10 @@
-class OnceCheckJob < ApplicationJob
-  queue_as :default
+# class OnceCheckJob < ApplicationJob
+#   queue_as :default
 
-  def perform(task)
-    if task.frequency.downcase == 'once'
-      if task.completeness_level.downcase == 'done'
-        # delete task
-    end
-  end
-end
+#   def perform(task)
+#     if task.frequency.downcase == 'once'
+#       if task.completeness_level.downcase == 'done'
+#         # delete task
+#     end
+#   end
+# end
--- planda2/app/worker.rb
+++ planda2/app/worker.rb
@@ -1,40 +1,40 @@
-require 'sidekiq'
+# require 'sidekiq'
 
 
-Sidekiq.configure_client do |config|
-  config.redis = { db: 1 }
-end
+# Sidekiq.configure_client do |config|
+#   config.redis = { db: 1 }
+# end
 
 
-Sidekiq.configure_server do |config|
-  config.redis = { db: 1 }
-end
+# Sidekiq.configure_server do |config|
+#   config.redis = { db: 1 }
+# end
 
 
-class dailyCheck
-  include Sidekiq::Worker
+# class dailyCheck
+#   include Sidekiq::Worker
 
-  def perform (task)
-    if task.frequency == 'daily'
-      if task.completeness == 'done'
-        task.completed++
-      end
-      task.completeness=='not started'
-      task.cycles++
-    end
-  end
-end
+#   def perform (task)
+#     if task.frequency == 'daily'
+#       if task.completeness == 'done'
+#         task.completed++
+#       end
+#       task.completeness=='not started'
+#       task.cycles++
+#     end
+#   end
+# end
 
-class weeklyCheck
-  include Sidekiq::Worker
+# class weeklyCheck
+#   include Sidekiq::Worker
 
-  def perform (task)
-    if task.frequency == 'weekly'
-      if task.completeness == 'done'
-        task.completed++
-      end
-      task.completeness=='not started'
-      task.cycles++
-    end
-  end
-end
+#   def perform (task)
+#     if task.frequency == 'weekly'
+#       if task.completeness == 'done'
+#         task.completed++
+#       end
+#       task.completeness=='not started'
+#       task.cycles++
+#     end
+#   end
+# end
